69d93a3eaf6a13064be124cba91803757eedc460
lums69 <josegsv28@gmail.com>
2021-11-08 19:07:08 -0400

    first commit

---
+++ src/components/Calculator/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container, Screen, Previous, Current } from './Styled';
+
+export default function Calculator() {
+    return(
+        <Container>
+            <Screen>
+                <Previous>10 +</Previous>
+                <Current>10</Current>
+            </Screen>
+        </Container>
+    )
+}
